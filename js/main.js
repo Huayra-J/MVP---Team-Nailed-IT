ca3498d266f2a3aa044e424a714ece62f9cf1cf4
Jerry Desktop <jerry.wangrj@gmail.com>
2019-05-09 12:07:39 +0930

    move script code to individual file Signed-off-by: Jerry Desktop <jerry.wangrj@gmail.com>

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function openNav() {
+	document.getElementById("open").style.display = "none";
+	document.getElementById("nav").style.width = "100%";
+}
+function closeNav() {
+	document.getElementById("nav").style.width = "0";
+	document.getElementById("open").style.display = "block";
+}
